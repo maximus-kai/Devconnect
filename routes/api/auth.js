f1c7aeb2fe0bf4463f1e99b0fdc010055b8adcb6
Dairo Tomiwa <38611128+maximus-kai@users.noreply.github.com>
2022-01-23 00:09:25 +0100

    auth route now shows user detail

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //@route    Get api/auth
 //@desc     Test Route
 //@access   Public
-router.get('/', (req,res)=> res.send('auth route'));
+router.get('/', auth, async (req,res)=> {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
+});
 
 module.exports = router;
